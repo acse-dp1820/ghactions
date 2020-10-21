73b59d2b4e3f9025770c2f20635045200346dd1e
Devang Patel <dp1820@ic.ac.uk>
2020-10-21 15:56:55 +0100

    edits

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-def my_sum(a,b):
+def my_sum(a, b):
     return a + b
 
-for i in [1,2,3,4]:
-    for j in [5,6,7,8]:
-        print(my_sum(i,j))
+
+for i in [1, 2, 3, 4]:
+    for j in [5, 6, 7, 8]:
+        print(my_sum(i, j))
